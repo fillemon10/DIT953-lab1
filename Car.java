14e496ba602d9ad05db055d6b19888fa0e485c17
fillemon10 <filip.sjolander@gmail.com>
2021-11-05 10:36:03 +0100

    car superclass

--- Car.java
+++ Car.java
@@ -1,3 +1,57 @@
-public class Car {
-    
+import java.awt.*;
+
+public abstract class Car {
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public double speedFactor() {
+        return 0;
+    }
+
+    public void incrementSpeed(double amount){
+
+    }
+
+    public void decrementSpeed(double amount){
+
+    }
+
+        // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
 }
