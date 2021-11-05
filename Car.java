2a19cf6f63d9547410c181a33f2cda7a3c4f4f02
Karl Kjellmer <karl.kjellmer@gmail.com>
2021-11-05 10:48:54 +0100

    Update Car.java
    from public to protected

--- Car.java
+++ Car.java
@@ -1,14 +1,17 @@
 import java.awt.*;
 
 public abstract class Car {
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
+
+
     public int getNrDoors(){
         return nrDoors;
     }
+
     public double getEnginePower(){
         return enginePower;
     }
@@ -20,6 +23,8 @@ public abstract class Car {
     public Color getColor(){
         return color;
     }
+
+    public String getModelName(){ return modelName; }
 
     public void setColor(Color clr){
         color = clr;
