6ef70f6015b5025dbdecc7735cb382eae052e9e6
Karl Kjellmer <karl.kjellmer@gmail.com>
2021-11-05 11:47:52 +0100

    angle

--- MovementVector2D.java
+++ MovementVector2D.java
@@ -1,6 +1,7 @@
 public class MovementVector2D {
     public double xDirection;
     public double yDirection;
+    public double angle;
     public MovementVector2D(double x,double y){
         this.xDirection = x;
         this.yDirection = y;
